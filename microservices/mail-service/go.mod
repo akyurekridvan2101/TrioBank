e3f6201b1fd27042cb6e6a477d2157ed718e178a
Samet Öztürk <abdulsamedozturk00830@gmail.com>
2025-12-10 13:37:25 +0300

    feat: resend mail api was used to send mail

--- microservices/mail-service/go.mod
+++ microservices/mail-service/go.mod
@@ -3,3 +3,5 @@ module github.com/TrioBank/triobank-platform/microservices/mail-service
 go 1.25.2
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/resend/resend-go/v3 v3.0.0 // indirect
--- microservices/mail-service/internal/handler.go
+++ microservices/mail-service/internal/handler.go
@@ -9,44 +9,19 @@ import (
 )
 
 func (s *SmtpPool) sendMailHandler(w http.ResponseWriter, r *http.Request) {
-
-	var requestBody string
 	var receiver Receiver
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
 	err := json.NewDecoder(r.Body).Decode(&receiver)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	requestBody = r.Header.Get("X-Request-Deadline")
-	timeoutInt64, err := strconv.ParseInt(requestBody, 10, 64)
+	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+	defer cancel() 
+	err = SendMail(ctx, receiver)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte("Invalid context deadline header"))
 		return
 	}
-
-	ctx, cancel := context.WithDeadline(r.Context(), time.UnixMilli(timeoutInt64))
-	defer cancel()
-	channel := make(chan error)
-	go SendMail(ctx, channel, s, receiver)
-
-	select {
-	case err = <-channel:
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			_, _ = w.Write([]byte("the mail could not be sent"))
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		return
-	case <-ctx.Done():
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte("email sending timed out or cancelled"))
-		return
-	}
+	w.WriteHeader(http.StatusOK)
 
 }
